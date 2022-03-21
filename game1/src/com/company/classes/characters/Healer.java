5e9a6c8f43da1d8bfab55d76683872e4521caf04
MaciekJ022 <maciek022@onet.pl>
2022-03-21 15:04:57 +0100

    Dodawanie gry. Wersja 1.0

---
+++ game1/src/com/company/classes/characters/Healer.java
@@ -0,0 +1,44 @@
+package com.company.classes.characters;
+
+import com.company.classes.AttackType;
+import com.company.classes.CharacterClass;
+
+public class Healer  extends CharacterClass {
+    public Healer(String name, int x, int y, int leftKey, int rightKey, int upKey, int downKey, int leftAttackKey, int rightAttackKey) {
+        super(name, x, y, leftKey, rightKey, upKey, downKey, leftAttackKey, rightAttackKey);
+        this.setLevel(1);
+        this.setMaxHealthPoints(1000);
+        this.setHealthPoints(1000);
+        this.setManaPoints(200);
+        this.setMaxManaPoints(200);
+        this.setAttackType(AttackType.PHYSICAL);
+        this.setAttackAmount(5);
+        this.setName(name);
+
+        this.setX(0);
+        this.setY(300);
+        this.uploadImage("1.png", "2.png", "3.png");
+    }
+    public void left(){
+        int newPositionX = getX() > 40 ? this.getX() - 40 : 320;
+        tryChangePosition(newPositionX,this.getY());
+    }
+    public void right(){
+        int newPositionX = getX() > 40 ? this.getX() + 40 : 320;
+        tryChangePosition(newPositionX,this.getY());
+    }
+    public void up(){
+        int newPositionY = getY() > 40 ? this.getY() - 40 : 320;
+        tryChangePosition(this.getX(),newPositionY);
+    }
+    public void down(){
+        int newPositionY = getY() > 40 ? this.getY() + 40 : 320;
+        tryChangePosition(this.getX(),newPositionY);
+    }
+    public void leftAttack(){
+
+    }
+    public void rightAttack(){
+
+    }
+}
